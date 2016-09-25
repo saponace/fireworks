88ce1b68112981cd2c6c616eabd42f19d5d76587
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2016-09-25 19:39:54 +0200

    Moved patterns to dedicated directory, and refactored script

--- fireworks.sh
+++ fireworks.sh
@@ -1,9 +1,13 @@
-
 #!/bin/bash
 
-for ((i = 0; i <= 58 ; i += 1))
+
+pattern_files_extension=".txt"
+patterns_dir="patterns"
+nb_frames=58
+
+for ((i = 0; i <= ${nb_frames} ; i += 1))
 do
-    cat $i.txt
-    sleep 0.03
+    cat ${patterns_dir}/${i}${pattern_files_extension}
+    sleep 0.04
     clear
 done
